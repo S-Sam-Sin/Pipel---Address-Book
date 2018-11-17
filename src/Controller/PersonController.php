8129653af6f922ed98e07a0ef857a3ba5f418f93
Omar Sam-Sin <sayid@nexlevel.nl>
2018-11-17 14:16:10 +0100

    Added webpack and encore and install semantic ui

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/person")
+ * @Route("/")
  */
 class PersonController extends AbstractController
 {
